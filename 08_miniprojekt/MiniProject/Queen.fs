879ad592539b50ee959786acdfda2b0c57f2e10a
Jan Schefer <jan.schefer95@gmail.com>
2024-07-02 09:52:35 +0200

    implement threat check

--- 08_miniprojekt/MiniProject/Queen.fs
+++ 08_miniprojekt/MiniProject/Queen.fs
@@ -1,5 +1,17 @@
 ﻿module Queen
 
+type BoardSolution = int[]
+
+let bedroht pos (teilloesung: BoardSolution) =
+    let row = Array.length teilloesung
+    let rec check (r: int) =
+        if r >= row then false
+        else
+            let c = teilloesung.[r]
+            if c = pos || abs (c - pos) = abs (r - row) then true
+            else check (r + 1)
+    check 0
+
 [<EntryPoint>]
 let main argv =
     printfn "Hello, World!" 
